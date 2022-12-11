2e625f4244895d3b8f25b92ce7f348ea78b28d01
Pavel Zubkov <zubkov_p@mail.ru>
2022-12-11 21:26:06 +0300

    Addable select

--- debug.view.css.ts
+++ debug.view.css.ts
@@ -1,32 +1,15 @@
 namespace $.$$ {
 
-	const { rem } = $mol_style_unit
+	const { rem, px } = $mol_style_unit
 
 	$mol_style_define( $my_debug, {
-		Source_page: {
+
+		Skills: {
 			flex: {
-				shrink: 0,
-				basis: rem(20),
+				grow: 0,
 			},
 		},
-		Code_page: {
-			flex: {
-				shrink: 0,
-				basis: rem(25),
-			},
-		},
-		Style_page: {
-			flex: {
-				shrink: 0,
-				basis: rem(20),
-			},
-		},
-		Preview_page: {
-			flex: {
-				shrink: 0,
-				basis: rem(15),
-			},
-		},
+
 	} )
 
 }
--- debug.view.ts
+++ debug.view.ts
@@ -3,77 +3,12 @@ namespace $.$$ {
 	export class $my_debug extends $.$my_debug {
 
 		@ $mol_mem
-		source(next?: string) {
-			return this.$.$mol_state_local.value('source', next) ?? super.source()
-		}
-
-		@ $mol_mem
-		tree() {
-			const source = this.source().replace( /\n?$/, '\n' )
-			
-			const tree = this.$.$mol_view_tree2_normalize(
-				this.$.$mol_tree2_from_string( source )
-			).kids[0]
-			
-			return tree
-		}
-
-		@ $mol_mem
-		code() {
-
-			const tree = this.tree()
-			
-			const code = this.$.$mol_tree2_text_to_string_mapped_js(
-				this.$.$mol_tree2_js_to_text(
-					this.$.$mol_view_tree2_to_js(
-						tree.list([ tree ])
-					)
-				)
-			)
-
-			return code
-
-		}
-
-		@ $mol_mem
-		preview() {
-			const View = this.$.$mol_js_eval( `${this.code()}; \n return $my_test` ) as typeof $mol_view
-			const view = View.Root(0)
-			const node = view.dom_node()
-			view.autorun()
-			return node
-		}
-
-		@ $mol_mem
-		css_node() {
-			const id = '$my_test'
-			const doc = $mol_dom_context.document
-			let el = doc.getElementById(id)
-			if (!el) {
-				el = doc.createElement('style')
-				el.id = id
-				doc.head.appendChild( el )
-			}
-			return el
-		}
-
-		@ $mol_mem
-		css(next?: string) {
-			const str = this.$.$mol_state_local.value('css', next) ?? super.css()
-			this.css_node().innerHTML = str
-			return str
-		}
-
-		prop_name(name: string) {
-			return name
-		}
-
-		props() {
-			const tree = this.tree()
-			const props = this.$.$mol_view_tree2_class_props( tree )
-			return tree.list( props ).kids.map( node => this.Prop( node.type ) )
+		dict1(next?: any) {
+			console.log(next)
+			return next ?? {} as any
 		}
 
 	}
 
 }
+					
