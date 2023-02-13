ac3ba7d20c56b91b94ae9baaef78ca025dff980b
Pavel Zubkov <zubkov_p@mail.ru>
2023-02-13 20:35:34 +0300

    100500

---
+++ debug.view.css.ts
@@ -0,0 +1,22 @@
+namespace $.$$ {
+
+	$mol_style_define($my_debug, {
+
+		padding: $mol_gap.block,
+
+		Item: {
+			border: {
+				radius: $mol_gap.round,
+			},
+			padding: $mol_gap.text,
+
+			':hover': {
+				background: {
+					color: $mol_theme.hover,
+				},
+			},
+		},
+
+	})
+
+}
--- debug.view.ts
+++ debug.view.ts
@@ -2,14 +2,14 @@ namespace $.$$ {
 
 	export class $my_debug extends $.$my_debug {
 
-		click() {
-			this.log()
+		@ $mol_mem
+		rows() {
+			return $mol_range2( index => index, () => this.items_count() ).map((index) => this.Item(index))
 		}
 
-		@ $mol_mem
-		log() {
-			$mol_wire_solid()
-			console.log(this.value())
+		@ $mol_mem_key
+		label(index: number) {
+			return super.label(index).replace('{index}', `${index + 1}`)
 		}
 
 	}
